8cbcc78817f0de96557101e8f9cb5f7f67516a49
H. Nikolaus Schaller <hns@goldelico.com>
2013-03-02 07:58:16 +0100

    improved blanviewd

--- debian/config-b2/etc/init.d/blanviewd.sh
+++ debian/config-b2/etc/init.d/blanviewd.sh
@@ -5,23 +5,23 @@
 # it will read the ambient light sensor and dim
 # the backlight intensity in bright environmental light
 #
-
+exit 1
 # Some things that run always
 
 # Carry out specific functions when asked to by the system
 case "$1" in
-  start)
-	cd /root
-	[ -x blanviewd ] || make blanviewd
-	blanviewd&
-    ;;
-  stop)
-	killall blanviewd
-    ;;
-  *)
-    echo "Usage: /etc/init.d/blah {start|stop}"
-    exit 1
-    ;;
+ start)
+       cd /root
+       [ -x blanviewd ] || make blanviewd
+       ./blanviewd &
+   ;;
+ stop)
+       killall blanviewd
+   ;;
+ *)
+   echo "Usage: /etc/init.d/blanviewd.sh {start|stop}"
+   exit 1
+   ;;
 esac
 
 exit 0
--- debian/config-b2/root/blanviewd.c
+++ debian/config-b2/root/blanviewd.c
@@ -5,6 +5,7 @@
  * backlight intensity for Ortustech Blanview display
  *
  * (c) H. N. Schaller, Golden Delicious Comp. GmbH&Co. KG, 2013
+ *     Lukas Märdian <lukas@goldelico.com>
  * Licence: GNU GPL2
  */
 
@@ -12,22 +13,12 @@
 #include <string.h>
 #include <errno.h>
 
-struct
-{
-	unsigned short value;
-	unsigned char backlight;
-} list[]=
-{
-	{ 0, 100 },		// fully on
-	{ 200, 100 },	// on
-	// add interpolation curve values
-	{ 4096, 0 },	// fully off
-	{ 65535, 0 },	// fully off
-};
-
 int main(int argc, char *argv[])
 {
 	char debug=0;
+	int prev_bl=-1;
+	char *file;
+	FILE *in, *out;
 	if(argv[1] != NULL && strcmp(argv[1], "-d") == 0)
 		{
 		fprintf(stderr, "debug mode\n");
@@ -36,70 +27,89 @@ int main(int argc, char *argv[])
 	else if(argv[1])
 		{
 		fprintf(stderr, "usage: blanviewd [-d]\n");
-		return 0;
+		return 1;
+		}
+#if 1
+	file="/sys/devices/platform/pwm-backlight/backlight/pwm-backlight/brightness";
+#else
+	file="/sys/devices/platform/pwm-backlight/backlight/pwm-backlight/max_brightness";
+#endif
+	out=fopen(file, "w");
+	if(!out)
+		{
+		fprintf(stderr, "%s: %s\n", file, strerror(errno));
+		return 1;
 		}
 	while(1)
 		{
-		char *file="/sys/bus/i2c/drivers/tsc2007/2-0048/values";
-		FILE *f=fopen(file, "r");
 		int i, n;
 		unsigned short x, y, pressure_raw, pressure, pendown;
 		unsigned short temperature, temp0, temp1;
 		unsigned short z1, z2;
 		unsigned short aux;
-		if(!f)
+		unsigned short ambient_light;
+		unsigned short bl;
+		file="/sys/bus/i2c/drivers/tsc2007/2-0048/values";
+		in=fopen(file, "r");
+		if(!in)
 			{
 			fprintf(stderr, "%s: %s\n", file, strerror(errno));
 			return 1;
 			}
-		n=fscanf(f, "%hu,%hu,%hu,%hu,%hu,%hu,%hu,%hu,%hu,%hu,%hu", 
-			   &x,
-			   &y,
-			   &pressure_raw,
-			   &pendown,
-			   &temperature,
-			   &z1,
-			   &z2,
-			   &temp0,
-			   &temp1,
-			   &aux,
-			   &pressure);
-		fclose(f);
-//		fprintf(stderr, "n=%d\n", n);
+		n=fscanf(in, "%hu,%hu,%hu,%hu,%hu,%hu,%hu,%hu,%hu,%hu,%hu", 
+				 &x,
+				 &y,
+				 &pressure_raw,
+				 &pendown,
+				 &temperature,
+				 &z1,
+				 &z2,
+				 &temp0,
+				 &temp1,
+				 &aux,
+				 &pressure);
+		fclose(in);
 		if(n == 11)
 			{
 			if(debug)
 				fprintf(stderr, "aux=%d", aux);
-			for(i=1; i<sizeof(list)/sizeof(list[0]); i++)
+			
+			if(aux <= 4096 && aux > 2048)	// max ambient light
+				ambient_light = 1;
+			else if(aux <= 2048 && aux > 1024)
+				ambient_light = 2;
+			else if(aux <= 1024 && aux > 512)
+				ambient_light = 3;
+			else if(aux <= 512 && aux > 256)
+				ambient_light = 4;
+			else if(aux <= 256 && aux > 0)	// min ambient light
+				ambient_light = 5;
+			else							// something went wrong, go max
+				ambient_light = 5;
+			
+			bl = ambient_light*20;
+			if(debug)
+				fprintf(stderr, " ->");
+			while(prev_bl != bl)
 				{
-				if(list[i].value > aux)
-					{ // found, interpolate
-						long db=list[i].backlight-list[i-1].backlight;
-						long dv=list[i].value-list[i-1].value;
-						int bl=list[i-1].backlight+(db*aux-list[i-1].value)/dv;	// interpolate
-#if 1
-						file="/sys/devices/platform/pwm-backlight/backlight/pwm-backlight/brightness";
-#else
-						file="/sys/devices/platform/pwm-backlight/backlight/pwm-backlight/max_brightness";
-#endif
-						f=fopen(file, "w");
-						if(!f)
-							{
-							fprintf(stderr, "%s: %s\n", file, strerror(errno));
-							return 1;
-							}
-						fprintf(f, "%d\n", bl);	// set backlight level
-						fclose(f);
-						if(debug)
-							fprintf(stderr, " -> %d\n", bl);
-						break;
-					}
+				if(prev_bl < 0)
+					prev_bl=bl;	// first
+				else if(prev_bl < bl)
+					prev_bl+=1;
+				else
+					prev_bl-=1;
+				fprintf(out, "%d\n", prev_bl);	// fade backlight level
+				fflush(out);
+				if(debug)
+					fprintf(stderr, " %d", prev_bl);
+				usleep(10000);
 				}
-			if(!(i<sizeof(list)/sizeof(list[0])))
-				fprintf(stderr, " -> ?\n");
+			prev_bl = bl;
+			if(debug)
+				fprintf(stderr, "\n");
 			}
 		else if(debug)
-			fprintf(stderr, "n=%d\n", n);
-		sleep(1);
+			fprintf(stderr, "n=%d\n", n);	// driver mismatch...
+		sleep(5); // next sample in 5 sec
 		}
 }
