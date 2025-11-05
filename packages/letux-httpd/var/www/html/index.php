59c6323dbe821d71dd9ba4da7ee1b490cbb98a43
H. Nikolaus Schaller <hns@goldelico.com>
2025-11-05 15:12:59 +0100

    letux-httpd: optionally report MySQL/MariaDB; report device, kernel, Debian
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- packages/letux-httpd/var/www/html/index.php
+++ packages/letux-httpd/var/www/html/index.php
@@ -1,6 +1,9 @@
 <h1>Welcome to Letux httpd installation</h1>
 
-Here you have PHP (letux-http) and optionally MySQL/MariaDB (letux-amp).
+<p>Here you have PHP (letux-http)<?php if(file_exists("/usr/bin/mysql")) echo " and MySQL/MariaDB (letux-amp)"; ?></p>
+<p>Device: <?php htmlentities(system("cat /proc/device-tree/model")); ?></p>
+<p>Kernel: <?php htmlentities(system("/bin/uname -a")); ?></p>
+<p>Debian: <?php htmlentities(system("cat /etc/debian_version")); ?></p>
 
 <?php
 
