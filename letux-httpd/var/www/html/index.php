c4296d058d8efe3c8f4851e26e6a7b8ccca3c31e
H. Nikolaus Schaller <hns@goldelico.com>
2026-04-27 21:24:31 +0200

    letux-httpd: multiple improvements in several places
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- letux-httpd/var/www/html/index.php
+++ letux-httpd/var/www/html/index.php
@@ -1,9 +1,25 @@
 <h1>Welcome to Letux httpd installation</h1>
+<p>Here you have PHP (letux-httpd)<?php if(file_exists("/usr/bin/mysql")) echo " and MySQL/MariaDB (letux-amp)"; ?></p>
+<p><a href="https://www.php.net/manual/">PHP Manual</a></p>
 
-<p>Here you have PHP (letux-http)<?php if(file_exists("/usr/bin/mysql")) echo " and MySQL/MariaDB (letux-amp)"; ?></p>
-<p>Device: <?php htmlentities(system("cat /proc/device-tree/model")); ?></p>
-<p>Kernel: <?php htmlentities(system("/bin/uname -a")); ?></p>
-<p>Debian: <?php htmlentities(system("cat /etc/debian_version")); ?></p>
+<?php
+
+$KERNEL=shell_exec("uname -a");
+
+if(file_exists("/proc/device-tree/model"))
+	$DEVICE=file_get_contents("/proc/device-tree/model");
+else
+	$DEVICE=shell_exec("uname -n");
+
+if(file_exists("/etc/debian_version"))
+	$OS="Debian ".file_get_contents("/etc/debian_version");
+else
+	$OS=shell_exec("uname -sr");
+?>
+
+<p>Device: <?php echo htmlentities($DEVICE); ?></p>
+<p>Kernel: <?php echo htmlentities($KERNEL); ?></p>
+<p>OS: <?php echo htmlentities($OS); ?></p>
 
 <?php
 
